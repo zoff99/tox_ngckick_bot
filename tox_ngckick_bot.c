8bafa577b56c3633d4652a2e0668a2b396f2e33e
zoff99 <zoff99@users.noreply.github.com>
2025-02-15 17:25:33 +0100

    do not forget to bootstrap

--- tox_ngckick_bot.c
+++ tox_ngckick_bot.c
@@ -847,13 +847,15 @@ static void bootstrap_tox(Tox *tox)
     // ----- bootstrap -----
     dbg(CLL_INFO, "Tox bootstrapping\n");
     // dummy node to bootstrap
-    tox_bootstrap(tox, "local", 7766, (uint8_t *)"2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1", NULL);
     for (int i = 0; nodes1[i].ip; i++) {
         uint8_t *key = (uint8_t *)calloc(1, 100);
         if (!key) {
             continue;
         }
         hex_string_to_bin2(nodes1[i].key, key);
+        if (nodes1[i].udp_port != 0) {
+            tox_bootstrap(tox, nodes1[i].ip, nodes1[i].udp_port, key, NULL);
+        }
         if (nodes1[i].tcp_port != 0) {
             tox_add_tcp_relay(tox, nodes1[i].ip, nodes1[i].tcp_port, key, NULL);
         }
@@ -934,6 +936,7 @@ int main(int argc, char *argv[])
     create_db();
 
     Tox *tox = create_tox();
+    bootstrap_tox(tox);
     tox_self_set_name(tox, (uint8_t *)"ToxNgckickBot", strlen("ToxNgckickBot"), NULL);
     update_tox_savedata(tox);
 
